60ae812f758ad490a59362fbca1e886dd22bad5e
Frazier Mork <fraziermork@gmail.com>
2016-04-06 10:34:10 -0700

    switched to including angular in the bundle file instead of having a separate script tag

--- entry.js
+++ entry.js
@@ -1,2 +1,3 @@
+require(__dirname + '/node_modules/angular/angular.js');
 require('!style!css!./style.css');
-require(__dirname + '/controllers/main-controller.js')
+require(__dirname + '/controllers/main-controller.js');
